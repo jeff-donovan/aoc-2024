d7a568f96355f30d81b876d4a9f40681159f9d44
Jeff Donovan <jdonovan@mariners.com>
2024-12-03 22:07:25 -0800

    setup 4.2.py

--- 4/4.2.py
+++ 4/4.2.py
@@ -1,73 +1,112 @@
 '''
 --- Part Two ---
-The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+The Elf looks quizzically at you. Did you misunderstand the assignment?
 
-The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+Looking for the instructions, you flip over the word search to find that this isn't actually an XMAS puzzle; it's an X-MAS puzzle in which you're supposed to find two MAS in the shape of an X. One way to achieve that is like this:
 
-Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+M.S
+.A.
+M.S
+Irrelevant characters have again been replaced with . in the above diagram. Within the X, each MAS can be written forwards or backwards.
 
-More of the above example's reports are now safe:
+Here's the same example from before, but this time all of the X-MASes have been kept instead:
 
-7 6 4 2 1: Safe without removing any level.
-1 2 7 8 9: Unsafe regardless of which level is removed.
-9 7 6 2 1: Unsafe regardless of which level is removed.
-1 3 2 4 5: Safe by removing the second level, 3.
-8 6 4 4 1: Safe by removing the third level, 4.
-1 3 6 7 9: Safe without removing any level.
-Thanks to the Problem Dampener, 4 reports are actually safe!
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
+In this example, an X-MAS appears 9 times.
 
-Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+Flip the word search from the instructions back over to the word search side and try again. How many times does an X-MAS appear?
 '''
 
-def get_reports(contents):
-    return [[int(n) for n in report.split(' ') if n != ''] for report in contents.split('\n') if report]
+def num_xmas(matrix):
+    all_a_coords = get_all_a_coords(matrix)
+    return sum([1 for a_coords in all_a_coords if is_xmas(matrix, a_coords)])
 
-def num_safe_reports(reports):
-    num = 0
-    for report in reports:
-        if report:
-            if is_safe(report):
-                num += 1
-            else:
-                for i in range(len(report)):
-                    report_copy = report.copy()
-                    report_copy.pop(i)
-                    is_safe_without_level = is_safe(report_copy)
-                    if is_safe_without_level:
-                        num += 1
-                        break
-    return num
+def get_all_a_coords(matrix):
+    all_a_coords = []
+    for i, row in enumerate(matrix):
+        for j, char in enumerate(row):
+            if char == 'A':
+                all_a_coords.append((i, j))
+    return all_a_coords
 
-def is_safe(report):
-    return is_diff_in_range(report) and (is_increasing(report) or is_decreasing(report))
+def make_matrix(contents):
+    return [row for row in contents.split('\n') if row]
 
-def is_increasing(report):
-    for i, level in enumerate(report):
-        if i != 0:
-            if level <= report[i - 1]:
-                return False
-    return True
+def is_xmas(matrix, a_coords):
+    return (
+        (is_mas_diagonal_down(matrix, a_coords) or is_sam_diagonal_down(matrix, a_coords)) and
+        (is_mas_diagonal_up(matrix, a_coords) or is_sam_diagonal_up(matrix, a_coords))
+    )
 
-def is_decreasing(report):
-    for i, level in enumerate(report):
-        if i != 0:
-            if level >= report[i - 1]:
-                return False
-    return True
+def is_mas_diagonal_down(matrix, a_coords):
+    pass
 
-def is_diff_in_range(report):
-    for i, level in enumerate(report):
-        if i != 0:
-            diff = abs(level - report[i - 1])
-            if not (1 <= diff <= 3):
-                return False
-    return True
+def is_sam_diagonal_down(matrix, a_coords):
+    pass
+
+def is_mas_diagonal_up(matrix, a_coords):
+    pass
+
+def is_sam_diagonal_up(matrix, a_coords):
+    pass
+
+# def num_xmas_diagonal(matrix, x_coords, is_backwards):
+#     i, j = x_coords
+
+#     num = 0
+
+#     # down and to the right
+#     if (i + 3 < len(matrix) and j + 3 < len(matrix)) and (
+#         matrix[i][j] == 'X'
+#         and matrix[i + 1][j + 1] == 'M'
+#         and matrix[i + 2][j + 2] == 'A'
+#         and matrix[i + 3][j + 3] == 'S'
+#     ):
+#         num += 1
+
+#     # down and to the left
+#     if (i + 3 < len(matrix) and j - 3 >= 0) and (
+#         matrix[i][j] == 'X'
+#         and matrix[i + 1][j - 1] == 'M'
+#         and matrix[i + 2][j - 2] == 'A'
+#         and matrix[i + 3][j - 3] == 'S'
+#     ):
+#         num += 1
+
+#     # up and to the left
+#     if (i - 3 >= 0 and j - 3 >= 0) and (
+#         matrix[i][j] == 'X'
+#         and matrix[i - 1][j - 1] == 'M'
+#         and matrix[i - 2][j - 2] == 'A'
+#         and matrix[i - 3][j - 3] == 'S'
+#     ):
+#         num += 1
+
+#     # up and to the right
+#     if (i - 3 >= 0 and j + 3 < len(matrix)) and (
+#         matrix[i][j] == 'X'
+#         and matrix[i - 1][j + 1] == 'M'
+#         and matrix[i - 2][j + 2] == 'A'
+#         and matrix[i - 3][j + 3] == 'S'
+#     ):
+#         num += 1
+
+#     return num
 
 
 if __name__ == '__main__':
-    with open('day_2_input.txt', 'r') as f:
+    with open('4/day_4_test.txt', 'r') as f:
         contents = f.read()
 
-    reports = get_reports(contents)
-    num = num_safe_reports(reports)
+    matrix = make_matrix(contents)
+    num = num_xmas(matrix)
     print(num)
