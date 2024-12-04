b506251812a0d1b275e9c74bc13564e6ab7222ac
Jeff Donovan <jdonovan@mariners.com>
2024-12-03 21:14:05 -0800

    update 4.1.py with logic

--- 4/4.1.py
+++ 4/4.1.py
@@ -1,78 +1,88 @@
 '''
---- Day 2: Red-Nosed Reports ---
-Fortunately, the first location The Historians want to search isn't a long walk from the Chief Historian's office.
+--- Day 4: Ceres Search ---
+"Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the only button on it. After a brief flash, you recognize the interior of the Ceres monitoring station!
 
-While the Red-Nosed Reindeer nuclear fusion/fission plant appears to contain no sign of the Chief Historian, the engineers there run up to you as soon as they see you. Apparently, they still talk about the time Rudolph was saved through molecular synthesis from a single electron.
+As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her word search (your puzzle input). She only has to find one word: XMAS.
 
-They're quick to add that - since you're already here - they'd really appreciate your help analyzing some unusual data from the Red-Nosed reactor. You turn to check if The Historians are waiting for you, but they seem to have already divided into groups that are currently searching every corner of the facility. You offer to help with the unusual data.
+This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. It's a little unusual, though, as you don't merely need to find one instance of XMAS - you need to find all of them. Here are a few ways XMAS might appear, where irrelevant characters have been replaced with .:
 
-The unusual data (your puzzle input) consists of many reports, one report per line. Each report is a list of numbers called levels that are separated by spaces. For example:
 
-7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9
-This example data contains six reports each containing five levels.
+..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....
+The actual word search will be full of letters instead. For example:
 
-The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+In this word search, XMAS occurs a total of 18 times; here's the same word search again, but where letters not involved in any XMAS have been replaced with .:
 
-The levels are either all increasing or all decreasing.
-Any two adjacent levels differ by at least one and at most three.
-In the example above, the reports can be found safe or unsafe by checking those rules:
-
-7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
-1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
-9 7 6 2 1: Unsafe because 6 2 is a decrease of 4.
-1 3 2 4 5: Unsafe because 1 3 is increasing but 3 2 is decreasing.
-8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
-1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
-So, in this example, 2 reports are safe.
-
-Analyze the unusual data from the engineers. How many reports are safe?
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+Take a look at the little Elf's word search. How many times does XMAS appear?
 '''
 
-def get_reports(contents):
-    return [[int(n) for n in report.split(' ') if n != ''] for report in contents.split('\n') if report]
+def num_xmas(matrix):
+    all_x_coords = get_all_x_coords(matrix)
+    return sum([1 for x_coords in all_x_coords if is_xmas(x_coords)])
 
-def num_safe_reports(reports):
-    num = 0
-    for report in reports:
-        if report and is_safe(report):
-            num += 1
-    return num
+def get_all_x_coords(matrix):
+    all_x_coords = []
+    for i, row in enumerate(matrix):
+        for j, char in enumerate(row):
+            if char == 'X':
+                all_x_coords.append((i, j))
+    return all_x_coords
 
-def is_safe(report):
-    return is_diff_in_range(report) and (is_increasing(report) or is_decreasing(report))
+def make_matrix(contents):
+    return [row for row in contents.split('\n') if row]
 
-def is_increasing(report):
-    for i, level in enumerate(report):
-        if i != 0:
-            if level <= report[i - 1]:
-                return False
-    return True
+def is_xmas(x_coords):
+    return (
+        is_xmas_horizontal(x_coords)
+        or is_xmas_vertical(x_coords)
+        or is_xmas_diagonal(x_coords)
+        or is_xmas_backwards(x_coords)
+        or is_xmas_overlapping(x_coords)
+    )
 
-def is_decreasing(report):
-    for i, level in enumerate(report):
-        if i != 0:
-            if level >= report[i - 1]:
-                return False
-    return True
+def is_xmas_horizontal(x_coords):
+    pass
 
-def is_diff_in_range(report):
-    for i, level in enumerate(report):
-        if i != 0:
-            diff = abs(level - report[i - 1])
-            if not (1 <= diff <= 3):
-                return False
-    return True
+def is_xmas_vertical(x_coords):
+    pass
+
+def is_xmas_diagonal(x_coords):
+    pass
+
+def is_xmas_backwards(x_coords):
+    pass
+
+def is_xmas_overlapping(x_coords):
+    pass
 
 
 if __name__ == '__main__':
-    with open('day_2_input.txt', 'r') as f:
+    with open('4/day_4_test.txt', 'r') as f:
         contents = f.read()
 
-    reports = get_reports(contents)
-    num = num_safe_reports(reports)
+    matrix = make_matrix(contents)
+    num = num_xmas(matrix)
     print(num)
