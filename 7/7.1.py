8a85d77a36241ce968aa2dc35b2d9cadc5177a1a
Jeff Donovan <jdonovan@mariners.com>
2024-12-06 21:37:29 -0800

    solve 7.1

--- 7/7.1.py
+++ 7/7.1.py
@@ -1,165 +1,94 @@
 '''
---- Day 6: Guard Gallivant ---
-The Historians use their fancy device again, this time to whisk you all away to the North Pole prototype suit manufacturing lab... in the year 1518! It turns out that having direct access to history is very convenient for a group of historians.
+--- Day 7: Bridge Repair ---
+The Historians take you to a familiar rope bridge over a river in the middle of a jungle. The Chief isn't on this side of the bridge, though; maybe he's on the other side?
 
-You still have to be careful of time paradoxes, and so it will be important to avoid anyone from 1518 while The Historians search for the Chief. Unfortunately, a single guard is patrolling this part of the lab.
+When you go to cross the bridge, you notice a group of engineers trying to repair it. (Apparently, it breaks pretty frequently.) You won't be able to cross until it's fixed.
 
-Maybe you can work out where the guard will go ahead of time so that The Historians can search safely?
+You ask how long it'll take; the engineers tell you that it only needs final calibrations, but some young elephants were playing nearby and stole all the operators from their calibration equations! They could finish the calibrations if only someone could determine which test values could possibly be produced by placing any combination of operators into their calibration equations (your puzzle input).
 
-You start by making a map (your puzzle input) of the situation. For example:
+For example:
 
-....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#...
-The map shows the current position of the guard with ^ (to indicate the guard is currently facing up from the perspective of the map). Any obstructions - crates, desks, alchemical reactors, etc. - are shown as #.
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+Each line represents a single equation. The test value appears before the colon on each line; it is your job to determine whether the remaining numbers can be combined with operators to produce the test value.
 
-Lab guards in 1518 follow a very strict patrol protocol which involves repeatedly following these steps:
+Operators are always evaluated left-to-right, not according to precedence rules. Furthermore, numbers in the equations cannot be rearranged. Glancing into the jungle, you can see elephants holding two different types of operators: add (+) and multiply (*).
 
-If there is something directly in front of you, turn right 90 degrees.
-Otherwise, take a step forward.
-Following the above protocol, the guard moves up several times until she reaches an obstacle (in this case, a pile of failed suit prototypes):
+Only three of the above equations can be made true by inserting operators:
 
-....#.....
-....^....#
-..........
-..#.......
-.......#..
-..........
-.#........
-........#.
-#.........
-......#...
-Because there is now an obstacle in front of the guard, she turns right before continuing straight in her new facing direction:
+190: 10 19 has only one position that accepts an operator: between 10 and 19. Choosing + would give 29, but choosing * would give the test value (10 * 19 = 190).
+3267: 81 40 27 has two positions for operators. Of the four possible configurations of the operators, two cause the right side to match the test value: 81 + 40 * 27 and 81 * 40 + 27 both equal 3267 (when evaluated left-to-right)!
+292: 11 6 16 20 can be solved in exactly one way: 11 + 6 * 16 + 20.
+The engineers just need the total calibration result, which is the sum of the test values from just the equations that could possibly be true. In the above example, the sum of the test values for the three equations listed above is 3749.
 
-....#.....
-........>#
-..........
-..#.......
-.......#..
-..........
-.#........
-........#.
-#.........
-......#...
-Reaching another obstacle (a spool of several very long polymers), she turns right again and continues downward:
-
-....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#......v.
-........#.
-#.........
-......#...
-This process continues for a while, but the guard eventually leaves the mapped area (after walking past a tank of universal solvent):
-
-....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#........
-........#.
-#.........
-......#v..
-By predicting the guard's route, you can determine which specific positions in the lab will be in the patrol path. Including the guard's starting position, the positions visited by the guard before leaving the area are marked with an X:
-
-....#.....
-....XXXXX#
-....X...X.
-..#.X...X.
-..XXXXX#X.
-..X.X.X.X.
-.#XXXXXXX.
-.XXXXXXX#.
-#XXXXXXX..
-......#X..
-In this example, the guard will visit 41 distinct positions on your map.
-
-Predict the path of the guard. How many distinct positions will the guard visit before leaving the mapped area?
+Determine which equations could possibly be true. What is their total calibration result?
 '''
 
-import pprint
 
+def make_mapping(contents):
+    mapping = []
+    for line in contents.split('\n'):
+        if line:
+            test_value, numbers = line.split(': ')
+            mapping.append((int(test_value), [int(n) for n in numbers.split(' ')]))
+    return mapping
 
-def get_num_positions_visited(matrix):
-    obstacles = get_obstacles(matrix)
+def make_operator_combos(len_numbers):
+    combos = []
+    for i in range(len_numbers):
+        if i == 0:
+            combos.append(['add',])
+            combos.append(['multiply',])
+            continue
 
-    direction = (-1, 0)  # up
-    current_i, current_j = get_initial_position(matrix)
+        if i == len_numbers - 1:
+            break
 
-    positions_visited = set([])
-    while is_on_map(matrix, current_i, current_j):
-        positions_visited.add((current_i, current_j))
+        new_combos = []
+        for combo in combos:
+            new_combos.append(combo + ['add',])
+            new_combos.append(combo + ['multiply',])
+        combos = new_combos
 
-        # traverse the map
-        next_i = current_i + direction[0]
-        next_j = current_j + direction[1]
+    return combos
 
-        while is_obstacle(obstacles, next_i, next_j):
-            direction = change_direction(direction[0], direction[1])
-            next_i = current_i + direction[0]
-            next_j = current_j + direction[1]
+def calculate_combo_total(numbers, operator_combo):
+    total = numbers[0]
+    for i in range(len(operator_combo)):
+        operator = operator_combo[i]
+        number = numbers[i + 1]
+        if operator == 'add':
+            total += number
+        if operator == 'multiply':
+            total = total * number
+    return total
 
-        current_i = next_i
-        current_j = next_j
+def can_numbers_produce_test_value(test_value, numbers):
+    combos = make_operator_combos(len(numbers))
+    for combo in combos:
+        if test_value == calculate_combo_total(numbers, combo):
+            return True
+    return False
 
-    return len(positions_visited)
-
-def is_safe(matrix, obstacles, i, j):
-    return is_on_map(matrix, i, j) and not is_obstacle(obstacles, i, j)
-
-def is_obstacle(obstacles, i, j):
-    return (i, j) in obstacles
-
-def is_on_map(matrix, i, j):
-    return (0 <= i < len(matrix)) and (0 <= j < len(matrix[i]))
-
-def get_obstacles(matrix):
-    obstacles = set([])
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if matrix[i][j] == '#':
-                obstacles.add((i, j))
-    return obstacles
-
-def get_initial_position(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if matrix[i][j] == '^':
-                return (i, j)
-
-def make_matrix(contents):
-    return [row for row in contents.split('\n') if row]
-
-def change_direction(old_direction_i, old_direction_j):
-    if old_direction_i == 1:  # down
-        return (0, -1)  # go left
-
-    if old_direction_i == -1:  # up
-        return (0, 1)  # go right
-
-    if old_direction_j == 1:  # right
-        return (1, 0)  # go down
-
-    if old_direction_j == -1:  # left
-        return (-1, 0)  # go up
+def get_total_calibration(mapping):
+    num = 0
+    for test_value, numbers in mapping:
+        if can_numbers_produce_test_value(test_value, numbers):
+            num += test_value
+    return num
 
 
 if __name__ == '__main__':
-    with open('6/day_6_input.txt', 'r') as f:
+    with open('7/day_7_test.txt', 'r') as f:
         contents = f.read()
 
-    matrix = make_matrix(contents)
-    num = get_num_positions_visited(matrix)
+    mapping = make_mapping(contents)
+    num = get_total_calibration(mapping)
     print(num)
