ebefa7b2bd3f4a8b762c84dd604389b97f289a54
Jeff Donovan <jdonovan@mariners.com>
2024-12-10 21:51:35 -0800

    refactor: cache stone -> blink(stone) results

--- 11/11.2.py
+++ 11/11.2.py
@@ -14,35 +14,44 @@ def make_stones(contents):
 def num_stones_for_blinks(stones, blinks):
     current = datetime.now()
 
+    cache = {}
+
     total_stones = 0
     for stone in stones:
         initial = [stone]
         for i in range(blinks):
-            initial = blink(initial)
+            initial = blink(cache, initial)
             print('blink ', i, ': ', datetime.now() - current)
             current = datetime.now()
 
         total_stones += len(initial)
     return total_stones
 
-def blink(stones):
+def blink(cache, stones):
     new_stones = []
     for stone in stones:
-        new_stones.extend(blink_for_stone(stone))
-
+        new_stones.extend(blink_for_stone(cache, stone))
     return new_stones
 
-def blink_for_stone(stone):
+def blink_for_stone(cache, stone):
+    if stone in cache:
+        return cache[stone]
+
+    result = []
     string_stone = str(stone)
     if stone == 0:
-        return [1]
+        result = [1]
 
-    if len(string_stone) % 2 == 0:
+    elif len(string_stone) % 2 == 0:
         left_stone = int(string_stone[:len(string_stone) // 2])
         right_stone = int(string_stone[len(string_stone) // 2:])
-        return [left_stone, right_stone]
+        result = [left_stone, right_stone]
 
-    return [stone * 2024]
+    else:
+        result = [stone * 2024]
+
+    cache[stone] = result
+    return result
 
 if __name__ == '__main__':
     with open('11/day_11_input.txt', 'r') as f:
