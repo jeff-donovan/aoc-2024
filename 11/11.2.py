b7b1969bc35c1ba57d106f79d73b8f5658cb608d
Jeff Donovan <jdonovan@mariners.com>
2024-12-10 22:16:39 -0800

    refactor with recursion

--- 11/11.2.py
+++ 11/11.2.py
@@ -5,53 +5,31 @@ The Historians sure are taking a long time. To be fair, the infinite corridors a
 How many stones would you have after blinking a total of 75 times?
 '''
 
-from datetime import datetime
-
 
 def make_stones(contents):
     return [int(string_stone) for string_stone in contents.split(' ') if string_stone]
 
 def num_stones_for_blinks(stones, blinks):
-    current = datetime.now()
-
-    cache = {}
-
     total_stones = 0
     for stone in stones:
-        initial = [stone]
-        for i in range(blinks):
-            initial = blink(cache, initial)
-            print('blink ', i, ': ', datetime.now() - current)
-            current = datetime.now()
+        total_stones += recursive_blink_for_stone(stone, blinks)
 
-        total_stones += len(initial)
     return total_stones
 
-def blink(cache, stones):
-    new_stones = []
-    for stone in stones:
-        new_stones.extend(blink_for_stone(cache, stone))
-    return new_stones
+def recursive_blink_for_stone(stone, level):
+    if level == 0:
+        return 1
 
-def blink_for_stone(cache, stone):
-    if stone in cache:
-        return cache[stone]
-
-    result = []
-    string_stone = str(stone)
     if stone == 0:
-        result = [1]
+        return recursive_blink_for_stone(1, level - 1)
 
-    elif len(string_stone) % 2 == 0:
+    string_stone = str(stone)
+    if len(string_stone) % 2 == 0:
         left_stone = int(string_stone[:len(string_stone) // 2])
         right_stone = int(string_stone[len(string_stone) // 2:])
-        result = [left_stone, right_stone]
+        return recursive_blink_for_stone(left_stone, level - 1) + recursive_blink_for_stone(right_stone, level - 1)
 
-    else:
-        result = [stone * 2024]
-
-    cache[stone] = result
-    return result
+    return recursive_blink_for_stone(stone * 2024, level - 1)
 
 if __name__ == '__main__':
     with open('11/day_11_input.txt', 'r') as f:
