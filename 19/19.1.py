8bedfb4ecaff998f32687358bff879a0f2e9fb3f
Jeff Donovan <jdonovan@mariners.com>
2024-12-18 22:08:32 -0800

    refactor: memoize and check all towels at once, rather than towel by towel

--- 19/19.1.py
+++ 19/19.1.py
@@ -6,22 +6,35 @@ def make_towels_and_designs(contents):
 
     return towels, designs
 
-def is_design_possible(towels, towel, remaining_design):
-    # base case - all we have left is an exact towel match
-    if towel == remaining_design:
-        return True
+def is_design_possible(cache, towels, remaining_design):
+    if remaining_design in cache:
+        return cache[remaining_design]
 
-    # base case - the towel doesn't exist *at the start* of the design
-    if not remaining_design.startswith(towel):
+    # base case - all we have left is an exact towel match
+    for towel in towels:
+        if towel == remaining_design:
+            cache[remaining_design] = True
+            return True
+
+    # base case - no towel exists *at the start* of the design
+    remaining_designs_to_check = set([])
+    for towel in towels:
+        if remaining_design.startswith(towel):
+            next_remaining_design = remaining_design[len(towel):]
+            remaining_designs_to_check.add(next_remaining_design)
+
+    if len(remaining_designs_to_check) == 0:
+        cache[remaining_design] = False
         return False
 
-    # at this point we know the towel starts off the remaining design so we can truncate and check what's left
-    next_remaining_design = remaining_design[len(towel):]
-    return any([is_design_possible(towels, t, next_remaining_design) for t in towels])
+    # at this point we know a towel starts off the remaining design so we can check the truncated designs
+    cache[remaining_design] = any([is_design_possible(cache, towels, next_remaining_design) for next_remaining_design in remaining_designs_to_check])
+    return cache[remaining_design]
 
 if __name__ == '__main__':
-    with open('19/day_19_input.txt', 'r') as f:
+    with open('19/day_19_test.txt', 'r') as f:
         contents = f.read()
 
     towels, designs = make_towels_and_designs(contents)
-    print(sum([is_design_possible(towels, '', d) for d in designs]))
+    cache = {}
+    print(sum([is_design_possible(cache, towels, d) for d in designs]))
