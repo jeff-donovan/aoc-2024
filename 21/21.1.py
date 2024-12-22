991de44b25975b51305506bad2d72b31ea7c0a5c
Jeff Donovan <jdonovan@mariners.com>
2024-12-21 22:50:36 -0800

    set up 21

--- 21/21.1.py
+++ 21/21.1.py
@@ -1,40 +1,12 @@
-def make_towels_and_designs(contents):
-    towels_contents, designs_contents = contents.split('\n\n')
+import pprint
 
-    towels = [t for t in towels_contents.split(', ') if t]
-    designs = [d for d in designs_contents.split('\n') if d]
 
-    return towels, designs
-
-def is_design_possible(cache, towels, remaining_design):
-    if remaining_design in cache:
-        return cache[remaining_design]
-
-    # base case - all we have left is an exact towel match
-    for towel in towels:
-        if towel == remaining_design:
-            cache[remaining_design] = True
-            return True
-
-    # base case - no towel exists *at the start* of the design
-    remaining_designs_to_check = set([])
-    for towel in towels:
-        if remaining_design.startswith(towel):
-            next_remaining_design = remaining_design[len(towel):]
-            remaining_designs_to_check.add(next_remaining_design)
-
-    if len(remaining_designs_to_check) == 0:
-        cache[remaining_design] = False
-        return False
-
-    # at this point we know a towel starts off the remaining design so we can check the truncated designs
-    cache[remaining_design] = any([is_design_possible(cache, towels, next_remaining_design) for next_remaining_design in remaining_designs_to_check])
-    return cache[remaining_design]
+def make_codes(contents):
+    return [[char for char in line] for line in contents.split('\n') if line]
 
 if __name__ == '__main__':
-    with open('19/day_19_input.txt', 'r') as f:
+    with open('21/day_21_input.txt', 'r') as f:
         contents = f.read()
 
-    towels, designs = make_towels_and_designs(contents)
-    cache = {}
-    print(sum([is_design_possible(cache, towels, d) for d in designs]))
+    codes = make_codes(contents)
+    pprint.pprint(codes)
