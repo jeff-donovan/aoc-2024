64b03c93f6a905df5ed946dda2fda218df2220f6
Jeff Donovan <jdonovan@mariners.com>
2024-12-22 21:40:14 -0800

    solve 23.2 for test

--- 23/23.2.py
+++ 23/23.2.py
@@ -18,10 +18,30 @@ def is_lan_party(connections, combo):
                 return False
     return True
 
+def largest_lan_party_for_group(connections, group):
+    if is_lan_party(connections, group):
+        return group
+
+    largest_lan_party = set([])
+    for computer in group:
+        new_group = set(group)
+        new_group.remove(computer)
+        new_lan_party = largest_lan_party_for_group(connections, new_group)
+        if len(new_lan_party) > len(largest_lan_party):
+            largest_lan_party = new_lan_party
+    return largest_lan_party
+
 if __name__ == '__main__':
     with open('23/day_23_test.txt', 'r') as f:
         contents = f.read()
 
     connections = parse_input(contents)
+    # sorted_connections = dict(sorted(connections.items(), key=lambda item: len(item[1])))
 
-    print(is_lan_party(connections, set(['co', 'de', 'ta', 'ka'])))
+    largest_lan_party = set([])
+    for computer in connections:
+        group = set(list(connections[computer]) + [computer])
+        new_lan_party = largest_lan_party_for_group(connections, group)
+        if len(new_lan_party) > len(largest_lan_party):
+            largest_lan_party = new_lan_party
+    print(','.join(sorted(list(largest_lan_party))))
