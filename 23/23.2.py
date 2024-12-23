49a8a45c363b4a1a258c9fea4ee990c2f33a1c28
Jeff Donovan <jdonovan@mariners.com>
2024-12-22 21:47:41 -0800

    solve with cache

--- 23/23.2.py
+++ 23/23.2.py
@@ -11,37 +11,47 @@ def parse_input(contents):
             connections[b].add(a)
     return connections
 
-def is_lan_party(connections, combo):
+def is_lan_party(cache, connections, combo):
+    key = cache_key(combo)
+    if key in cache:
+        return cache[key]
+
     for computer in combo:
         for other_computer in combo:
             if (computer != other_computer) and computer not in connections[other_computer]:
+                cache[key] = False
                 return False
+    cache[key] = True
     return True
 
-def largest_lan_party_for_group(connections, group):
-    if is_lan_party(connections, group):
+def largest_lan_party_for_group(cache, connections, group):
+    if is_lan_party(cache, connections, group):
         return group
 
     largest_lan_party = set([])
     for computer in group:
         new_group = set(group)
         new_group.remove(computer)
-        new_lan_party = largest_lan_party_for_group(connections, new_group)
+        new_lan_party = largest_lan_party_for_group(cache, connections, new_group)
         if len(new_lan_party) > len(largest_lan_party):
             largest_lan_party = new_lan_party
     return largest_lan_party
 
+def cache_key(combo_set):
+    return tuple(sorted(combo_set))
+
 if __name__ == '__main__':
-    with open('23/day_23_input.txt', 'r') as f:
+    with open('23/day_23_test.txt', 'r') as f:
         contents = f.read()
 
     connections = parse_input(contents)
     # sorted_connections = dict(sorted(connections.items(), key=lambda item: len(item[1])))
 
+    cache = {}
     largest_lan_party = set([])
     for computer in connections:
         group = set(list(connections[computer]) + [computer])
-        new_lan_party = largest_lan_party_for_group(connections, group)
+        new_lan_party = largest_lan_party_for_group(cache, connections, group)
         if len(new_lan_party) > len(largest_lan_party):
             largest_lan_party = new_lan_party
             print('new largest lan party: ', largest_lan_party)
