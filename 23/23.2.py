076ba99dc7047d82bcd3b5954c4f846622e37053
Jeff Donovan <jdonovan@mariners.com>
2024-12-22 22:03:19 -0800

    remove print statements

--- 23/23.2.py
+++ 23/23.2.py
@@ -51,19 +51,13 @@ if __name__ == '__main__':
     start = datetime.datetime.now()
 
     connections = parse_input(contents)
-    # sorted_connections = dict(sorted(connections.items(), key=lambda item: len(item[1])))
-    print('finished setting up connections in ', datetime.datetime.now() - start)
 
     cache = {}
-    # is_lan_party_cache = {}
     largest_lan_party = set([])
     for computer in connections:
         group = set(list(connections[computer]) + [computer])
-        print('group: ', group)
         new_lan_party = largest_lan_party_for_group(cache, connections, group)
-        print(f'largest lan party for computer {computer} is: {new_lan_party}')
-        print(f'finished setting up largest lan party for {computer} in ', datetime.datetime.now() - start)
         if len(new_lan_party) > len(largest_lan_party):
             largest_lan_party = new_lan_party
-            print('new largest lan party: ', largest_lan_party)
     print(','.join(sorted(list(largest_lan_party))))
+    print('finished in ', datetime.datetime.now() - start)
