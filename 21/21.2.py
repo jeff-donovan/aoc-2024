307bed128f61b505c60956cba0a2d4399a281db5
Jeff Donovan <jdonovan@mariners.com>
2024-12-22 23:43:49 -0800

    compare inputs

--- 21/21.2.py
+++ 21/21.2.py
@@ -177,7 +177,7 @@ def calculate_complexity(code, sequences):
     return calculate_min_path_length(sequences) * int(code[:len(code) - 1])
 
 if __name__ == '__main__':
-    with open('21/day_21_test.txt', 'r') as f:
+    with open('21/day_21_input.txt', 'r') as f:
         contents = f.read()
 
     codes = make_codes(contents)
