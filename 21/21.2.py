078de718545b396260adede2692b62c750d20e18
Jeff Donovan <jdonovan@mariners.com>
2024-12-22 23:50:12 -0800

    switch logic to track depth

--- 21/21.2.py
+++ 21/21.2.py
@@ -106,40 +106,25 @@ def calculate_min_path_length(paths):
 
 # update algorithm logic to keep track of min sequence length at each depth
 # only fan out for sequences with the min sequence length
-def find_all_sequences(code):
-    seq_num_to_dir = numerical_to_direction(code)
+def find_all_sequences(code, depth):
+    sequences = numerical_to_direction(code)
 
-    seq_dir_to_dir = []
-    min_length = None
-    for seq in seq_num_to_dir:
-        new_paths = directional_to_directional(seq)
-        first_path_length = len(new_paths[0])
-        if min_length is None or first_path_length < min_length:
-            min_length = first_path_length
+    for _ in range(depth):
+        new_sequences = []
+        min_length = None
+        for seq in sequences:
+            new_paths = directional_to_directional(seq)
+            first_path_length = len(new_paths[0])
+            if min_length is None or first_path_length < min_length:
+                min_length = first_path_length
 
-        for path in new_paths:
-            if len(path) <= min_length:
-                seq_dir_to_dir.append(path)
-    # seq_dir_to_dir = [path for path in seq_dir_to_dir if len(path) == calculate_min_path_length(seq_dir_to_dir)]
+            for path in new_paths:
+                if len(path) <= min_length:
+                    new_sequences.append(path)
 
-    final_seq = []
-    min_length = None
-    for seq in seq_dir_to_dir:
-        # final_seq.extend(directional_to_directional(seq))
-        new_paths = directional_to_directional(seq)
-        first_path_length = len(new_paths[0])
-        if min_length is None or first_path_length < min_length:
-            min_length = first_path_length
+        sequences = new_sequences
 
-        for path in new_paths:
-            if len(path) <= min_length:
-                final_seq.append(path)
-    # final_seq = [path for path in final_seq if len(path) == calculate_min_path_length(final_seq)]
-
-    return final_seq
-
-def find_a_shortest_sequence(code):
-    seq_num_to_dir = numerical_to_direction(code)
+    return sequences
 
 def numerical_to_direction(code):
     sequences = [[]]
@@ -183,17 +168,5 @@ if __name__ == '__main__':
     codes = make_codes(contents)
 
     start = datetime.datetime.now()
-    print(sum([calculate_complexity(code, find_all_sequences(code)) for code in codes]))
+    print(sum([calculate_complexity(code, find_all_sequences(code, 2)) for code in codes]))
     print('took ', datetime.datetime.now() - start)
-
-    # print(numerical_to_direction('029A'))
-    # for result in numerical_to_direction('029A'):
-
-    #     print(f'{len(result)}: ', result)
-
-    # for result in directional_to_directional(['<', 'A', '^', 'A', '^', '^', '>', 'A', 'v', 'v', 'v', 'A']):
-    # for result in directional_to_directional(['<', 'A', '^', 'A', '^', '>', '^', 'A', 'v', 'v', 'v', 'A']):
-    # for result in directional_to_directional(['<', 'A', '^', 'A', '>', '^', '^', 'A', 'v', 'v', 'v', 'A']):
-    # # for result in [first_result, second_result, third_result]:
-    #     print(f'{len(result)}: ', result)
-    #     print()
