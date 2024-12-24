967eea7365e8fe7aa2271a0a686902b37a52b1f9
Jeff Donovan <jdonovan@mariners.com>
2024-12-23 22:21:23 -0800

    set up logic

--- 24/24.1.py
+++ 24/24.1.py
@@ -1,33 +1,66 @@
 def parse_input(contents):
-    connections = {}
-    for line in contents.split('\n'):
-        if line:
-            a, b = line.split('-')
-            if a not in connections:
-                connections[a] = set([])
-            if b not in connections:
-                connections[b] = set([])
-            connections[a].add(b)
-            connections[b].add(a)
-    return connections
+    values_content, gates_content = contents.split('\n\n')
 
-def t_in_combo(combo):
-    return combo[0].startswith('t') or combo[1].startswith('t') or combo[2].startswith('t')
+    values = {}
+    for line in values_content.split('\n'):
+        if line:
+            k, v = line.split(': ')
+            values[k] = int(v)
+
+    gates = []
+    for gate in gates_content.split('\n'):
+        if gate:
+            command_data, z = gate.split(' -> ')
+            x, command, y = command_data.split(' ')
+            gates.append((command, x, y, z))
+
+    return values, gates
+
+def apply_gate(values, command, x, y, z):
+    if z in values:
+        return True
+
+    x_val = values.get(x)
+    y_val = values.get(y)
+
+    if x_val is None or y_val is None:
+        return False
+
+    if command == 'AND':
+        values[z] = gate_and(x_val, y_val)
+
+    if command == 'OR':
+        values[z] = gate_or(x_val, y_val)
+
+    if command == 'XOR':
+        values[z] = gate_xor(x_val, y_val)
+
+    return True
+
+def gate_and(x_val, y_val):
+    if x_val == 1 and y_val == 1:
+        return 1
+    return 0
+
+def gate_or(x_val, y_val):
+    if x_val == 1 or y_val == 1:
+        return 1
+    return 0
+
+def gate_xor(x_val, y_val):
+    if x_val != y_val:
+        return 1
+    return 0
 
 if __name__ == '__main__':
     with open('24/day_24_test.txt', 'r') as f:
         contents = f.read()
 
-    connections = parse_input(contents)
+    values, gates = parse_input(contents)
+    print('values: ', values)
+    print('gates: ', gates)
 
-    multi = set([])
-    for a in connections:
-        for b in connections:
-            for c in connections:
-                a_to_b = b in connections[a]
-                b_to_c = c in connections[b]
-                c_to_a = a in connections[c]
-                if a_to_b and b_to_c and c_to_a:
-                    combo = sorted([a, b, c])
-                    multi.add((combo[0], combo[1], combo[2]))
-    print(sum([1 for combo in multi if t_in_combo(combo)]))
+    for (command, x, y, z) in gates:
+        apply_gate(values, command, x, y, z)
+
+    print('values after: ', values)
