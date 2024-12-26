cbb3bcf12e19b01b1d6273a37002a02f831df0a6
Jeff Donovan <jdonovan@mariners.com>
2024-12-26 15:07:06 -0800

    add back in apply_* helpers from previous approach

--- 24/24.2_attempt3.py
+++ 24/24.2_attempt3.py
@@ -1,3 +1,4 @@
+import copy
 import itertools
 
 
@@ -18,6 +19,61 @@ def parse_input(contents):
             gates.append((command, x, y, z))
 
     return values, gates
+
+def apply_all_gates(values, gates):
+    copied_gates = copy.deepcopy(gates)
+    copied_values = copy.deepcopy(values)
+    i = 0
+    while len(copied_gates) > 0:
+        if i >= len(copied_gates):
+            raise Exception('not this time!')
+
+        command, x, y, z = copied_gates[i]
+        success = apply_gate(copied_values, command, x, y, z)
+        if not success:
+            i += 1
+            continue
+
+        copied_gates.pop(i)
+        i = 0
+
+    return copied_values, copied_gates
+
+def apply_gate(values, command, x, y, z):
+    if z in values:
+        return True
+
+    x_val = values.get(x)
+    y_val = values.get(y)
+
+    if x_val is None or y_val is None:
+        return False
+
+    if command == 'AND':
+        values[z] = gate_and(x_val, y_val)
+
+    if command == 'OR':
+        values[z] = gate_or(x_val, y_val)
+
+    if command == 'XOR':
+        values[z] = gate_xor(x_val, y_val)
+
+    return True
+
+def gate_and(x_val, y_val):
+    if x_val == 1 and y_val == 1:
+        return 1
+    return 0
+
+def gate_or(x_val, y_val):
+    if x_val == 1 or y_val == 1:
+        return 1
+    return 0
+
+def gate_xor(x_val, y_val):
+    if x_val != y_val:
+        return 1
+    return 0
 
 def get_all_values_combos(values):
     x_keys = get_all_x_keys(values)
@@ -45,3 +101,5 @@ if __name__ == '__main__':
 
     values, gates = parse_input(contents)
     print(len(get_all_values_combos(values)))
+    for values_combo in get_all_values_combos(values):
+        apply_all_gates(values_combo, gates)
