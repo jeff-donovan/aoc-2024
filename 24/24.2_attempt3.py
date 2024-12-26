68084defb013b6b90e1e3bb67283825c07b378c2
Jeff Donovan <jdonovan@mariners.com>
2024-12-26 14:59:53 -0800

    generate all system combinations

--- 24/24.2_attempt3.py
+++ 24/24.2_attempt3.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def parse_input(contents):
     values_content, gates_content = contents.split('\n\n')
 
@@ -16,8 +19,29 @@ def parse_input(contents):
 
     return values, gates
 
+def get_all_values_combos(values):
+    x_keys = get_all_x_keys(values)
+    y_keys = get_all_y_keys(values)
+    keys = x_keys + y_keys
+
+    result = []
+    for values_combo in itertools.product([0, 1], repeat=len(keys)):
+        combo = {}
+        for i in range(len(keys)):
+            combo[keys[i]] = values_combo[i]
+        result.append(combo)
+
+    return result
+
+def get_all_x_keys(values):
+    return sorted([key for key in values.keys() if key.startswith('x')])
+
+def get_all_y_keys(values):
+    return sorted([key for key in values.keys() if key.startswith('y')])
+
 if __name__ == '__main__':
     with open('C:/code/aoc-2024/24/day_24_test_part2.txt', 'r') as f:
         contents = f.read()
 
     values, gates = parse_input(contents)
+    print(len(get_all_values_combos(values)))
