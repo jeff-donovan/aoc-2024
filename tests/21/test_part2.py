8b013a546e41a339dd62ba683c2071d5f5f9a833
Jeff Donovan <jdonovan@mariners.com>
2024-12-26 22:06:40 -0800

    add test for directional_to_directional()

--- tests/21/test_part2.py
+++ tests/21/test_part2.py
@@ -4,9 +4,14 @@ import day_21.part2 as part2
 class TestPart2(unittest.TestCase):
 
     def test_group_by_A(self):
-        seq = '<A^A>^^AvvvA'
-        expected = ['<A', '^A', '>^^A', 'vvvA']
+        seq = ['<', 'A', '^', 'A', '>', '^', '^', 'A', 'v', 'v', 'v', 'A']
+        expected = [['<', 'A'], ['^', 'A'], ['>', '^', '^', 'A'], ['v', 'v', 'v', 'A']]
         self.assertEqual(part2.group_by_A(seq), expected)
+
+    def test_directional_to_directional(self):
+        seq = ['<', 'A', '^', 'A', '>', '^', '^', 'A', 'v', 'v', 'v', 'A']
+        expected = ['v', '<', '<', 'A', '>', '>', '^', 'A', '<', 'A', '>', 'A', 'v', 'A', '<', '^', 'A', 'A', '>', 'A', '<', 'v', 'A', 'A', 'A', '>', '^', 'A']
+        self.assertIn(expected, part2.directional_to_directional(seq))
 
 if __name__ == '__main__':
     unittest.main()
