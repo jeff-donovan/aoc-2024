933c37569659de80340bd5cff09230a64bbf0b06
Jeff Donovan <jdonovan@mariners.com>
2024-12-26 22:59:12 -0800

    discovery around length of second level d_to_d

--- tests/21/test_part2.py
+++ tests/21/test_part2.py
@@ -13,6 +13,16 @@ class TestPart2(unittest.TestCase):
         expected = ['v', '<', '<', 'A', '>', '>', '^', 'A', '<', 'A', '>', 'A', 'v', 'A', '<', '^', 'A', 'A', '>', 'A', '<', 'v', 'A', 'A', 'A', '>', '^', 'A']
         self.assertIn(expected, part2.directional_to_directional(seq))
 
+    def test_directional_to_directional_by_depth(self):
+        initial_seq = ['<', 'A']
+        second_level_sequences = part2.directional_to_directional(initial_seq)
+        for i in range(len(second_level_sequences)):
+            third_level_sequences = part2.directional_to_directional(second_level_sequences[i])
+            print(f'{i} - {part2.calculate_min_path_length(third_level_sequences)}')
+            for seq in third_level_sequences:
+                fourth_level_sequences = part2.directional_to_directional(seq)
+                print(f'{i} - {part2.calculate_min_path_length(fourth_level_sequences)}')
+
     def test_directional_to_directional_using_group_by_A(self):
         seq = ['<', 'A', '^', 'A', '>', '^', '^', 'A', 'v', 'v', 'v', 'A']
         expected = ['v', '<', '<', 'A', '>', '>', '^', 'A', '<', 'A', '>', 'A', 'v', 'A', '<', '^', 'A', 'A', '>', 'A', '<', 'v', 'A', 'A', 'A', '>', '^', 'A']
