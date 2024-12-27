2cf48eae4e3f6719f6675b86729ac5fc67d109a7
Jeff Donovan <jdonovan@mariners.com>
2024-12-26 17:09:53 -0800

    check for other mismatches

--- 24/24.2_attempt4.py
+++ 24/24.2_attempt4.py
@@ -30,13 +30,23 @@ def get_all_z_keys(gates):
     return sorted([gate[-1] for gate in gates if gate[-1].startswith('z')])
 
 if __name__ == '__main__':
-    with open('C:/code/aoc-2024/24/day_24_test_part2.txt', 'r') as f:
+    with open('C:/code/aoc-2024/24/day_24_input.txt', 'r') as f:
         contents = f.read()
 
     values, gates = parse_input(contents)
+
+    # check for any obvious z switches
     for z_key in get_all_z_keys(gates):
         inputs = get_xy_inputs(gates, z_key)
         z_index = int(z_key[1:])
         for input in inputs:
             if int(input[1:]) > z_index:
                 print(z_key)
+
+    # check for any x/y mismatches
+    for (_, x, y, output) in gates:
+        if (x.startswith('x') or y.startswith('x')):
+            if not (x.startswith('y') or y.startswith('y')):
+                print(output)
+            if x[1:] != y[1:]:
+                print(output)
