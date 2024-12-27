3127fd708be4896e0283c07690a544e692f9f734
Jeff Donovan <jdonovan@mariners.com>
2024-12-27 12:13:59 -0800

    new attempt

---
+++ day_21/part2_attempt2.py
@@ -0,0 +1,120 @@
+import copy
+import datetime
+import pprint
+
+
+NUMERICAL_KEYPAD = {
+    'A': {
+        '^': '3',
+        '<': '0',
+    },
+    '0': {
+        '^': '2',
+        '>': 'A',
+    },
+    '1': {
+        '^': '4',
+        '>': '2',
+    },
+    '2': {
+        '^': '5',
+        'v': '0',
+        '<': '1',
+        '>': '3',
+    },
+    '3': {
+        '^': '6',
+        'v': 'A',
+        '<': '2',
+    },
+    '4': {
+        '^': '7',
+        'v': '1',
+        '>': '5',
+    },
+    '5': {
+        '^': '8',
+        'v': '2',
+        '<': '4',
+        '>': '6',
+    },
+    '6': {
+        '^': '9',
+        'v': '3',
+        '<': '5',
+    },
+    '7': {
+        'v': '4',
+        '>': '8',
+    },
+    '8': {
+        'v': '5',
+        '<': '7',
+        '>': '9',
+    },
+    '9': {
+        'v': '6',
+        '<': '8',
+    },
+}
+
+DIRECTIONAL_KEYPAD = {
+    'A': {
+        '<': '^',
+        'v': '>'
+    },
+    '^': {
+        '>': 'A',
+        'v': 'v',
+    },
+    '<': {
+        '>': 'v',
+    },
+    '>': {
+        '<': 'v',
+        '^': 'A'
+    },
+    'v': {
+        '<': '<',
+        '>': '>',
+        '^': '^',
+    },
+}
+
+def make_codes(contents):
+    return [line for line in contents.split('\n') if line]
+
+def group_by_A(seq):
+    a_indices = [i for i, char in enumerate(seq) if char == 'A']
+    new_sequences = []
+    start = 0
+    for a_index in a_indices:
+        new_sequences.append(seq[start : a_index + 1])
+        start = a_index + 1
+    return new_sequences
+
+def tidy_up(sequences):
+    min_length = calculate_min_path_length(sequences)
+    return [seq for seq in sequences if len(seq) == min_length]
+
+def calculate_complexity(code, sequences):
+    return calculate_min_path_length(sequences) * int(code[:len(code) - 1])
+
+def calculate_min_path_length(paths):
+    return min([len(path) for path in paths])
+
+if __name__ == '__main__':
+    with open('day_21/day_21_test.txt', 'r') as f:
+        contents = f.read()
+
+    codes = make_codes(contents)
+    print(type(codes))
+
+    start = datetime.datetime.now()
+    print('took ', datetime.datetime.now() - start)
+
+# IDEAS
+#  - i think the "find the winner" approach was correct, BUT... as we go deeper levels in we should:
+#    - leverage our already calculated winners
+#    - group by A
+#    - DELETE losers as we go
