0da7d3902acee7282fbdae74ba84dffb45709cfb
Jeff Donovan <jdonovan@mariners.com>
2024-12-26 16:58:54 -0800

    check test for potential swaps

--- 24/24.2_attempt4.py
+++ 24/24.2_attempt4.py
@@ -30,7 +30,7 @@ def get_all_z_keys(gates):
     return sorted([gate[-1] for gate in gates if gate[-1].startswith('z')])
 
 if __name__ == '__main__':
-    with open('C:/code/aoc-2024/24/day_24_input.txt', 'r') as f:
+    with open('C:/code/aoc-2024/24/day_24_test_part2.txt', 'r') as f:
         contents = f.read()
 
     values, gates = parse_input(contents)
