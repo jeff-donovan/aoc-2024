113a366a6d2562bae3890105e2aa0293b62ecebe
Jeff Donovan <jdonovan@mariners.com>
2024-12-27 13:12:07 -0800

    switch group_by_A() to use strings

--- tests/21/test_part2_attempt2.py
+++ tests/21/test_part2_attempt2.py
@@ -5,8 +5,8 @@ import day_21.part2_attempt2 as part2_attempt2
 class TestPart2Attempt2(unittest.TestCase):
 
     def test_group_by_A(self):
-        seq = ['<', 'A', '^', 'A', '>', '^', '^', 'A', 'v', 'v', 'v', 'A']
-        expected = [['<', 'A'], ['^', 'A'], ['>', '^', '^', 'A'], ['v', 'v', 'v', 'A']]
+        seq = '<A^A>^^AvvvA'
+        expected = ['<A', '^A', '>^^A', 'vvvA']
         self.assertEqual(part2_attempt2.group_by_A(seq), expected)
 
     def test_find_shortest_paths_returns_list_of_strings(self):
