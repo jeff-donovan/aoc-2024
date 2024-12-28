ef6b02f603714e5a9fbc9f3f5601b6af1817d6b6
Jeff Donovan <jdonovan@mariners.com>
2024-12-27 22:51:24 -0800

    new (dumb) approach - TOO SLOW

---
+++ day_21/part2_attempt4.py
@@ -0,0 +1,166 @@
+import datetime
+
+
+NUMERICAL_KEYPAD = {
+    'A': {
+        '^': '3',
+        '<': '0',
+    },
+    '0': {
+        '^': '2',
+        '>': 'A',
+    },
+    '1': {
+        '^': '4',
+        '>': '2',
+    },
+    '2': {
+        '^': '5',
+        'v': '0',
+        '<': '1',
+        '>': '3',
+    },
+    '3': {
+        '^': '6',
+        'v': 'A',
+        '<': '2',
+    },
+    '4': {
+        '^': '7',
+        'v': '1',
+        '>': '5',
+    },
+    '5': {
+        '^': '8',
+        'v': '2',
+        '<': '4',
+        '>': '6',
+    },
+    '6': {
+        '^': '9',
+        'v': '3',
+        '<': '5',
+    },
+    '7': {
+        'v': '4',
+        '>': '8',
+    },
+    '8': {
+        'v': '5',
+        '<': '7',
+        '>': '9',
+    },
+    '9': {
+        'v': '6',
+        '<': '8',
+    },
+}
+
+DIRECTIONAL_KEYPAD = {
+    'A': {
+        '<': '^',
+        'v': '>'
+    },
+    '^': {
+        '>': 'A',
+        'v': 'v',
+    },
+    '<': {
+        '>': 'v',
+    },
+    '>': {
+        '<': 'v',
+        '^': 'A'
+    },
+    'v': {
+        '<': '<',
+        '>': '>',
+        '^': '^',
+    },
+}
+
+def make_codes(contents):
+    return [line for line in contents.split('\n') if line]
+
+def numerical_to_direction(cache, code):
+    sequences = ['']
+    for i in range(len(code)):
+        if i == 0:
+            start = 'A'
+        else:
+            start = code[i - 1]
+        end = code[i]
+        paths = find_shortest_paths(cache, NUMERICAL_KEYPAD, start, end)
+        new_sequences = []
+        for path in paths:
+            for seq in sequences:
+                new_sequences.append(seq + path)
+        sequences = new_sequences
+    return sequences
+
+def directional_to_directional(cache, directional_seq):
+    sequences = ['']
+    for i in range(len(directional_seq)):
+        if i == 0:
+            start = 'A'
+        else:
+            start = directional_seq[i - 1]
+        end = directional_seq[i]
+        paths = find_shortest_paths(cache, DIRECTIONAL_KEYPAD, start, end)
+        new_sequences = []
+        for path in paths:
+            for seq in sequences:
+                new_sequences.append(seq + path)
+        sequences = new_sequences
+    return sequences
+
+def find_shortest_paths(cache, keypad, start_char, end_char):
+    cache_key = start_char + end_char
+    if cache_key not in cache:
+        cache[cache_key] = _find_shortest_paths(keypad, start_char, end_char)
+    return cache[cache_key]
+
+def _find_shortest_paths(keypad, start_char, end_char, visited=None):
+    if visited is None:
+        visited = []
+
+    if start_char in visited:
+        return []
+
+    if start_char == end_char:
+        return ['A']
+
+    paths = []
+    for direction, next_start_char in keypad[start_char].items():
+        next_paths = [direction + path for path in _find_shortest_paths(keypad, next_start_char, end_char, visited + [start_char])]
+        paths += next_paths
+
+    return [path for path in paths if len(path) == calculate_min_path_length(paths)]
+
+def calculate_complexity(code, sequences):
+    return calculate_min_path_length(sequences) * int(code[:len(code) - 1])
+
+def calculate_min_path_length(paths):
+    return min([len(path) for path in paths])
+
+if __name__ == '__main__':
+    with open('day_21/day_21_test.txt', 'r') as f:
+        contents = f.read()
+
+    codes = make_codes(contents)
+
+    depth = 3
+    cache = {}
+    start = datetime.datetime.now()
+    complexities = []
+    for code in codes:
+        sequences = numerical_to_direction(cache, code)
+        for _ in range(depth):
+            new_sequences = []
+            for seq in sequences:
+                new_sequences.extend(directional_to_directional(cache, seq))
+            sequences = new_sequences
+        complexities.append(calculate_complexity(code, sequences))
+    print(sum(complexities))
+    print('took ', datetime.datetime.now() - start)
+    print()
