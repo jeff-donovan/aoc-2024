99bf90ac0a0c2856636b2bff460a9298384044da
Jeff Donovan <jdonovan@mariners.com>
2024-12-27 16:17:05 -0800

    new attempt - attempt 3

---
+++ day_21/part2_attempt3.py
@@ -0,0 +1,98 @@
+import datetime
+
+
+NUMERICAL_KEYPAD = {
+    'A': {
+        '^': '3',
+        '<': '0',
+    },
+    '0': {
+        '^': '2',
+        '>': 'A',
+    },
+    '1': {
+        '^': '4',
+        '>': '2',
+    },
+    '2': {
+        '^': '5',
+        'v': '0',
+        '<': '1',
+        '>': '3',
+    },
+    '3': {
+        '^': '6',
+        'v': 'A',
+        '<': '2',
+    },
+    '4': {
+        '^': '7',
+        'v': '1',
+        '>': '5',
+    },
+    '5': {
+        '^': '8',
+        'v': '2',
+        '<': '4',
+        '>': '6',
+    },
+    '6': {
+        '^': '9',
+        'v': '3',
+        '<': '5',
+    },
+    '7': {
+        'v': '4',
+        '>': '8',
+    },
+    '8': {
+        'v': '5',
+        '<': '7',
+        '>': '9',
+    },
+    '9': {
+        'v': '6',
+        '<': '8',
+    },
+}
+
+DIRECTIONAL_KEYPAD = {
+    'A': {
+        '<': '^',
+        'v': '>'
+    },
+    '^': {
+        '>': 'A',
+        'v': 'v',
+    },
+    '<': {
+        '>': 'v',
+    },
+    '>': {
+        '<': 'v',
+        '^': 'A'
+    },
+    'v': {
+        '<': '<',
+        '>': '>',
+        '^': '^',
+    },
+}
+
+def make_codes(contents):
+    return [line for line in contents.split('\n') if line]
+
+if __name__ == '__main__':
+    with open('day_21/day_21_input.txt', 'r') as f:
+        contents = f.read()
+
+    codes = make_codes(contents)
+
+    start = datetime.datetime.now()
+    cache = {}
+    print('took ', datetime.datetime.now() - start)
+
+# PLAN:
+#  - use recursion to continue finding "shortest path" of smaller sequences until either:
+#    - seq in cache: we've previously calculated the shortest path
+#    - there is a single A left
