9c2da19b1f0e390150a3aea1f80962b3f3423d59
Jeff Donovan <jdonovan@mariners.com>
2024-12-30 13:54:05 -0800

    parse file input, add num/direction maps

--- day_21/21.2.go
+++ day_21/21.2.go
@@ -6,6 +6,98 @@ import (
 	"os"
 )
 
+var NUMERICAL_KEYPAD = map[string]map[string]string{
+	"A": {
+		"^": "3",
+		"<": "0",
+	},
+	"0": {
+		"^": "2",
+		">": "A",
+	},
+	"1": {
+		"^": "4",
+		">": "2",
+	},
+	"2": {
+		"^": "5",
+		"v": "0",
+		"<": "1",
+		">": "3",
+	},
+	"3": {
+		"^": "6",
+		"v": "A",
+		"<": "2",
+	},
+	"4": {
+		"^": "7",
+		"v": "1",
+		">": "5",
+	},
+	"5": {
+		"^": "8",
+		"v": "2",
+		"<": "4",
+		">": "6",
+	},
+	"6": {
+		"^": "9",
+		"v": "3",
+		"<": "5",
+	},
+	"7": {
+		"v": "4",
+		">": "8",
+	},
+	"8": {
+		"v": "5",
+		"<": "7",
+		">": "9",
+	},
+	"9": {
+		"v": "6",
+		"<": "8",
+	},
+}
+
+var DIRECTIONAL_KEYPAD = map[string]map[string]string{
+	"A": {
+		"<": "^",
+		"v": ">",
+	},
+	"^": {
+		">": "A",
+		"v": "v",
+	},
+	"<": {
+		">": "v",
+	},
+	">": {
+		"<": "v",
+		"^": "A",
+	},
+	"v": {
+		"<": "<",
+		">": ">",
+		"^": "^",
+	},
+}
+
+func makeCodes(f *os.File) ([]string, error) {
+	var codes []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		codes = append(codes, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return codes, nil
+}
+
 func main() {
 	f, err := os.Open("day_21/day_21_test.txt")
 	if err != nil {
@@ -14,13 +106,10 @@ func main() {
 	}
 
 	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
+	codes, err := makeCodes(f)
+	if err != nil {
 		fmt.Println("Error reading file:", err)
 	}
+
+	fmt.Println("codes: ", codes)
 }
