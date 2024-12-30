dc59a62545228c103f250d8ff9d13c85b0ab9996
Jeff Donovan <jdonovan@mariners.com>
2024-12-30 15:50:57 -0800

    add numericalToDirectional(), Cache, etc.

--- day_21/21.2.go
+++ day_21/21.2.go
@@ -4,84 +4,112 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"sync"
 )
 
-var NUMERICAL_KEYPAD = map[string]map[string]string{
-	"A": {
-		"^": "3",
-		"<": "0",
+type Keypad map[rune]map[rune]rune
+
+var NUMERICAL_KEYPAD = Keypad{
+	'A': {
+		'^': '3',
+		'<': '0',
 	},
-	"0": {
-		"^": "2",
-		">": "A",
+	'0': {
+		'^': '2',
+		'>': 'A',
 	},
-	"1": {
-		"^": "4",
-		">": "2",
+	'1': {
+		'^': '4',
+		'>': '2',
 	},
-	"2": {
-		"^": "5",
-		"v": "0",
-		"<": "1",
-		">": "3",
+	'2': {
+		'^': '5',
+		'v': '0',
+		'<': '1',
+		'>': '3',
 	},
-	"3": {
-		"^": "6",
-		"v": "A",
-		"<": "2",
+	'3': {
+		'^': '6',
+		'v': 'A',
+		'<': '2',
 	},
-	"4": {
-		"^": "7",
-		"v": "1",
-		">": "5",
+	'4': {
+		'^': '7',
+		'v': '1',
+		'>': '5',
 	},
-	"5": {
-		"^": "8",
-		"v": "2",
-		"<": "4",
-		">": "6",
+	'5': {
+		'^': '8',
+		'v': '2',
+		'<': '4',
+		'>': '6',
 	},
-	"6": {
-		"^": "9",
-		"v": "3",
-		"<": "5",
+	'6': {
+		'^': '9',
+		'v': '3',
+		'<': '5',
 	},
-	"7": {
-		"v": "4",
-		">": "8",
+	'7': {
+		'v': '4',
+		'>': '8',
 	},
-	"8": {
-		"v": "5",
-		"<": "7",
-		">": "9",
+	'8': {
+		'v': '5',
+		'<': '7',
+		'>': '9',
 	},
-	"9": {
-		"v": "6",
-		"<": "8",
+	'9': {
+		'v': '6',
+		'<': '8',
 	},
 }
 
-var DIRECTIONAL_KEYPAD = map[string]map[string]string{
-	"A": {
-		"<": "^",
-		"v": ">",
+var DIRECTIONAL_KEYPAD = Keypad{
+	'A': {
+		'<': '^',
+		'v': '>',
 	},
-	"^": {
-		">": "A",
-		"v": "v",
+	'^': {
+		'>': 'A',
+		'v': 'v',
 	},
-	"<": {
-		">": "v",
+	'<': {
+		'>': 'v',
 	},
-	">": {
-		"<": "v",
-		"^": "A",
+	'>': {
+		'<': 'v',
+		'^': 'A',
 	},
-	"v": {
-		"<": "<",
-		">": ">",
-		"^": "^",
+	'v': {
+		'<': '<',
+		'>': '>',
+		'^': '^',
 	},
+}
+
+type Cache struct {
+	data  map[string][]string
+	mutex sync.RWMutex
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[string][]string),
+	}
+}
+
+func (c *Cache) Set(key string, value []string) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.data[key] = value
+}
+
+func (c *Cache) Get(key string) ([]string, bool) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	val, exists := c.data[key]
+	return val, exists
 }
 
 func makeCodes(f *os.File) ([]string, error) {
@@ -98,8 +126,82 @@ func makeCodes(f *os.File) ([]string, error) {
 	return codes, nil
 }
 
+func numericalToDirection(cache *Cache, code string) []string {
+	sequences := []string{""}
+	for i, end := range code {
+		var start rune
+		if i == 0 {
+			start = 'A'
+		} else {
+			start = rune(code[i-1])
+		}
+
+		var newSequences []string
+		for _, path := range findShortestPaths(cache, NUMERICAL_KEYPAD, start, end) {
+			for _, seq := range sequences {
+				newSequences = append(newSequences, seq+path)
+			}
+		}
+		sequences = newSequences
+	}
+	return sequences
+}
+
+func findShortestPaths(cache *Cache, keypad Keypad, start rune, end rune) []string {
+	var value []string
+	cacheKey := string(start) + string(end)
+	value, found := cache.Get(cacheKey)
+	if !found {
+		value = _findShortestPaths(keypad, start, end, "")
+		cache.Set(cacheKey, value)
+	}
+	return value
+}
+
+func _findShortestPaths(keypad Keypad, start rune, end rune, visited string) []string {
+	for _, char := range visited {
+		if char == start {
+			return nil
+		}
+	}
+
+	if start == end {
+		return []string{"A"}
+	}
+
+	var paths []string
+	for direction, next_start := range keypad[start] {
+		for _, path := range _findShortestPaths(keypad, next_start, end, visited+string(start)) {
+			paths = append(paths, string(direction)+path)
+		}
+	}
+
+	if len(paths) == 0 {
+		return paths
+	}
+
+	return tidyUp(paths)
+}
+
+func tidyUp(paths []string) []string {
+	minLength := calculateMinPathLength(paths)
+	var result []string
+	for _, path := range paths {
+		if len(path) == minLength {
+			result = append(result, path)
+		}
+	}
+	return result
+}
+
+func calculateMinPathLength(paths []string) int {
+	return len(slices.MinFunc(paths, func(a, b string) int {
+		return len(a) - len(b)
+	}))
+}
+
 func main() {
-	f, err := os.Open("day_21/day_21_test.txt")
+	f, err := os.Open("C:/code/aoc-2024/day_21/day_21_test.txt")
 	if err != nil {
 		fmt.Println("Error opening file", err)
 		return
@@ -111,5 +213,8 @@ func main() {
 		fmt.Println("Error reading file:", err)
 	}
 
-	fmt.Println("codes: ", codes)
+	cache := NewCache()
+	for _, code := range codes {
+		fmt.Println(numericalToDirection(cache, code))
+	}
 }
