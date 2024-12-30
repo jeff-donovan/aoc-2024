816a1d985ed89f484e3753cd35fbda3494b25f77
Jeff Donovan <jdonovan@mariners.com>
2024-12-30 13:32:21 -0800

    open test file, print each line

--- day_21/21.2.go
+++ day_21/21.2.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	f, err := os.Open("day_21/day_21_test.txt")
+	if err != nil {
+		fmt.Println("Error opening file", err)
+		return
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
 }
